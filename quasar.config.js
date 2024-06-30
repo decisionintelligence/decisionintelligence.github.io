a90ca92c8f0ba930f1e9a83f0546112b1b086237
xwyzsn <xwyzsn@gmail.com>
2024-07-01 00:01:23 +0800

    add i18n

--- quasar.config.js
+++ quasar.config.js
@@ -23,6 +23,9 @@ module.exports = configure(function (ctx) {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-webpack/boot-files
+    boot: [
+      'i18n'
+    ],
     boot: ["i18n"],
 
     // https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-css
