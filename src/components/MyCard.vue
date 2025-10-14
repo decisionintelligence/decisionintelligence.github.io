00915d9d6add673ab0ea1b22285b023bbd02aab4
Kongbaiyyue <1216855441@qq.com>
2025-10-14 11:59:32 +0800

    acc load

--- src/components/MyCard.vue
+++ src/components/MyCard.vue
@@ -3,29 +3,15 @@
     <!--    <a v-if="mode==='horizontal'" :href="link" target="_blank" class="tw-bg-white tw-w-full-->
     <!--    tw-rounded-lg tw-border tw-border-gray-200 tw-shadow-md" style="height: 20vw;">-->
     <!--      <div class=" tw-grid tw-grid-rows-5">-->
-    <div
-      style="min-height: 220px"
-      v-if="mode === 'horizontal'"
-      @click="goTo(link)"
-      class="tw-rounded-lg tw-border tw-border-gray-200 tw-h-full hover:tw-cursor-pointer"
-    >
-      <img
-        :src="require('../assets/img/' + picUrl)"
-        style="width: 150px; height: 200px"
-        class="tw-float-left tw-mr-4 tw-object-contain tw-mt-2"
-      />
-      <div
-        class="row tw-mt-2 tw-mb-2 tw-text-xl tw-font-bold tw-tracking-tight tw-text-gray-900"
-      >
+    <div style="min-height: 220px" v-if="mode === 'horizontal'" @click="goTo(link)"
+      class="tw-rounded-lg tw-border tw-border-gray-200 tw-h-full hover:tw-cursor-pointer">
+      <img :src="picUrl" @load="onLoad" style="width: 150px; height: 200px"
+        class="tw-float-left tw-mr-4 tw-object-contain tw-mt-2" />
+      <div class="row tw-mt-2 tw-mb-2 tw-text-xl tw-font-bold tw-tracking-tight tw-text-gray-900">
         {{ name }}
       </div>
       <div class="row tw-m-2">
-        <span
-          class="tw-text-gray-400 tw-mr-2"
-          v-for="(t, idx) in tag"
-          :key="idx"
-          >{{ t }}</span
-        >
+        <span class="tw-text-gray-400 tw-mr-2" v-for="(t, idx) in tag" :key="idx">{{ t }}</span>
       </div>
       <div class="tw-pl-2 tw-ml-2 tw-tracking-wider tw-leading-7">
         {{ desc }}
@@ -34,10 +20,13 @@
 
 
     <div v-else-if="mode === 'vertical'"
-      class="tw-bg-white tw-m-auto  tw-h-full  tw-border tw-border-gray-200 tw-text-center" style="max-width: 230px;" @click="link ? goTo(link) : null">
+      class="tw-bg-white tw-m-auto  tw-h-full  tw-border tw-border-gray-200 tw-text-center" style="max-width: 230px;"
+      @click="link ? goTo(link) : null">
       <img style="width:160px;height: 160px" v-if="type !== 'alumni'"
-        class="tw-object-contain tw-rounded-l-lg tw-m-auto tw-pt-1" :src="require('../assets/img/' + picUrl)">
+        class="tw-object-contain tw-rounded-l-lg tw-m-auto tw-pt-1" :src="picUrl" @load="onLoad">
       <div>
+
+        <!-- class="tw-object-contain tw-rounded-l-lg tw-m-auto tw-pt-1" :src="require('../assets/img/' + picUrl)"> -->
         <div class="tw-mt-1" :class="link ? 'tw-text-blue-500 hover:tw-cursor-pointer' : 'tw-text-gray-900'">
           {{ name }}
         </div>
@@ -70,9 +59,23 @@ export default {
   },
 
   setup(props) {
-    let picUrl = props.pic.split("/").at(-1);
+    // let picUrl = props.pic.split("/").at(-1);
+    let picUrl = "https://fastly.jsdelivr.net/gh/decisionintelligence/decisionintelligence.github.io@code/src/assets/img_low/" + props.pic.split("/").at(-1);
+    let lowResUrl = "https://fastly.jsdelivr.net/gh/decisionintelligence/decisionintelligence.github.io@code/src/assets/img_low/" + props.pic.split("/").at(-1);
+    let highResUrl = 'https://fastly.jsdelivr.net/gh/decisionintelligence/decisionintelligence.github.io@code/src/assets/img/' + props.pic.split("/").at(-1);
+    console.log(picUrl)
     function getAssets(url) {
       return new URL(url, import.meta.url).href;
+    }
+    function onLoad() {
+      // 如果当前加载的是 lowRes，就加载 highRes
+      if (picUrl === lowResUrl) {
+        const img = new Image()
+        img.src = highResUrl
+        img.onload = () => {
+          picUrl = highResUrl
+        }
+      }
     }
     const goTo = (link) => {
       window.open(link, "_blank");
